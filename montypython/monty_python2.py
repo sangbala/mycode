d295aba7e4ae8a2e36c366e3fc1f7bdb5d1ec3d9
sangbala <aaeuio@gmail.com>
2023-01-11 18:29:36 +0000

    while looping

--- montypython/monty_python2.py
+++ montypython/monty_python2.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 
 round=0
+answer=" "
 
 while True:
     
-    round=round+1
+    round=round+1 # increase the round counter by 1
     print('Finish the movie title, "Monty Python\'s The Life of ..."')
 
     answer=input("Your guess--> ")
+    answer=answer.upper()
+    
+    if answer=="SHRUBBERY":
+        print("You gave the super secret answer!")
+        break
 
-    if answer=="Brian":
+    elif answer=="BRIAN":  # logic to check if user gave correct answer
         print('Correct')
         break
 
-    elif round==3:
+    elif round==3:   # logic to ensure round has not yet reached 3
         print("Sorry, the answer was Brian.")
         break
 
-    else:
+    else: # if answer was wrong
         print("Sorry! Try again!")
 
