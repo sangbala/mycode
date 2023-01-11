0a8401f43d9ac855b6479af2b6946e10e3b239bc
sangbala <aaeuio@gmail.com>
2023-01-11 17:57:15 +0000

    while looping

---
+++ challenge42.py
@@ -0,0 +1,15 @@
+challenge= ["science", "turbo", ["goggles", "eyes"], "nothing"]
+
+
+trial= ["science", "turbo", {"eyes": "goggles", "goggles": "eyes"}, "nothing"]
+
+
+nightmare= [{"slappy": "a", "text": "b", "kumquat": "goggles", "user":{"awesome": "c", "name": {"first": "eyes", "last": "toes"}},"banana": 15, "d": "nothing"}]
+
+
+print("My "+challenge[2][1]+"! The "+challenge[2][0]+" do "+challenge[3]+"!")
+
+print("My "+trial[2]["goggles"]  +"! The "+trial[2]["eyes"]+" do "+trial[3]+"!")
+
+print("My "+nightmare[0]["user"]["name"]["first"]  +"! The "+nightmare[0]["kumquat"]+" do "+nightmare[0]["d"]+"!")
+
---
+++ challnege41.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+marvelchars= {
+"Starlord":
+  {"real name": "peter quill",
+  "powers": "dance moves",
+  "archenemy": "Thanos"},
+
+"Mystique":
+  {"real name": "raven darkholme",
+  "powers": "shape shifter",
+  "archenemy": "Professor X"},
+
+"Hulk":
+  {"real name": "bruce banner",
+  "powers": "super strength",
+  "archenemy": "adrenaline"}
+             }
+
+
+
+char_name=input('Which character do you want to know about? (Starlord, Mystique, Hulk)')
+
+char_stat=input('What statistic do you want to know about? (real name, powers, archenemy)')
+
+value=marvelchars[char_name.title()][char_stat.lower()]
+
+print(char_name+"'s "+char_stat+" is: "+value)
+
+
