1c55cf5e4c0130679469362ce23fca54f46ce3f0
sangbala <aaeuio@gmail.com>
2023-01-17 15:43:23 +0000

    1-17-23

---
+++ challenge61.py
@@ -0,0 +1,8 @@
+
+num_bottle=int(input("How many bottles"))
+range_num=num_bottle
+for rando in range(range_num):
+    
+    print(f'{num_bottle}'+"bottles of beer on the wall!")
+    num_bottle-=1
+
